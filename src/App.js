357b93cabd84475e6e10cf0824cbe78e71b5da42
Paul Figueroa <pjsfigueroa@gmail.com>
2023-03-29 11:52:43 +0800

    Add initial router setup.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "pages/home/HomePage";
+import LoginPage from "pages/login/LoginPage";
+import ProfilePage from "pages/profile/ProfilePage";
 
 function App() {
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
