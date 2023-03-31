4083c7f3893dff9e42803171f7ebb5a37812c2f2
Paul Figueroa <pjsfigueroa@gmail.com>
2023-03-31 08:38:37 +0800

    Fix server url in UserImage component.

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = '60px' }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:4000/assets/${image}`}
+        src={`${process.env.REACT_APP_SERVER_URL}/assets/${image}`}
       />
     </Box>
   )
